f1c87be8134cb3f43dd4b56ddeaead8ba3ea8270
yukisora <yukisora2013@gmail.com>
2016-09-22 04:38:00 +0800

    full size image view

--- app/src/main/java/moe/yukisora/yandere/ImageViewActivity.java
+++ app/src/main/java/moe/yukisora/yandere/ImageViewActivity.java
@@ -2,9 +2,12 @@ package moe.yukisora.yandere;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,25 +16,40 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class ImageViewActivity extends Activity {
+    private ImageData imageData;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
 
         Intent intent = this.getIntent();
-        ImageData imageData = (ImageData)intent.getSerializableExtra("imageData");
-        ImageView imageView = (ImageView)findViewById(R.id.fullSizeImageView);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        imageData = (ImageData)intent.getSerializableExtra("imageData");
+        imageView = (ImageView)findViewById(R.id.fullSizeImageView);
+        imageView.setImageResource(R.mipmap.ic_launcher);
 
-        StrictMode.setThreadPolicy(policy);
-        try {
-            URL url = new URL(imageData.file_url);
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-            connection.setConnectTimeout(3000);
-            connection.setReadTimeout(3000);
-            imageView.setImageBitmap(BitmapFactory.decodeStream(connection.getInputStream()));
-        } catch (IOException ignored) {
+        new DownloadImage().execute(imageData.file_url);
+    }
+
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                URLConnection connection = url.openConnection();
+                connection.setRequestProperty("User-Agent","Mozilla/5.0");
+                connection.setConnectTimeout(3000);
+                connection.setReadTimeout(3000);
+
+                return BitmapFactory.decodeStream(connection.getInputStream());
+            } catch (IOException e) {
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
         }
     }
 }

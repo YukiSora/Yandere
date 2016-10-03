ae5c902702e46c80430a055b05c33d6055869201
yukisora <yukisora2013@gmail.com>
2016-10-04 02:14:15 +0800

    download button

--- app/src/main/java/moe/yukisora/yandere/ImageViewActivity.java
+++ app/src/main/java/moe/yukisora/yandere/ImageViewActivity.java
@@ -32,17 +32,26 @@ public class ImageViewActivity extends Activity {
         imageView = (ImageView)findViewById(R.id.fullSizeImageView);
         imageView.setImageResource(R.drawable.placeholder_large);
 
+        String imageIdStr = String.format("yande.re Id: %d", imageData.id);
+        ((TextView)findViewById(R.id.imageId)).setText(imageIdStr);
         String imageSizeStr = String.format("Image Size: %d x %d", imageData.width, imageData.height);
         ((TextView)findViewById(R.id.imageSize)).setText(imageSizeStr);
         String fileSizeStr = String.format("File Size: %.2fkb", imageData.file_size / 1024f);
         ((TextView)findViewById(R.id.fileSize)).setText(fileSizeStr);
 
-        findViewById(R.id.download).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new SaveImageTask().start();
-            }
-        });
+        File file = new File(MainActivity.getDirectory(), "yandere_" + imageData.id + "." + imageData.file_ext);
+        if (file.exists()) {
+            ((ImageView)findViewById(R.id.downloadImage)).setImageResource(R.drawable.done);
+            findViewById(R.id.download).setEnabled(false);
+        }
+        else {
+            findViewById(R.id.download).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new SaveImageTask().start();
+                }
+            });
+        }
 
         new DownloadImageTask().start();
     }
@@ -64,26 +73,26 @@ public class ImageViewActivity extends Activity {
         @Override
         public void run() {
             File file = new File(MainActivity.getDirectory(), "yandere_" + imageData.id + "." + imageData.file_ext);
-            if (!file.exists()) {
-                Bitmap bitmap = ImageManager.downloadImage(imageData.file_url);
-                try (FileOutputStream out = new FileOutputStream(file)) {
-                    if (imageData.file_ext.equalsIgnoreCase("png"))
-                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
-                    else if (imageData.file_ext.equalsIgnoreCase("jpg"))
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                } catch (IOException ignore) {
-                }
-
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Image is downloaded.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                //display in photo gallery
-                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).setData(Uri.fromFile(file)));
+            Bitmap bitmap = ImageManager.downloadImage(imageData.file_url);
+            try (FileOutputStream out = new FileOutputStream(file)) {
+                if (imageData.file_ext.equalsIgnoreCase("png"))
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                else if (imageData.file_ext.equalsIgnoreCase("jpg"))
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            } catch (IOException ignore) {
             }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    ((ImageView)findViewById(R.id.downloadImage)).setImageResource(R.drawable.done);
+                    findViewById(R.id.download).setEnabled(false);
+                    Toast.makeText(getApplicationContext(), "Image is downloaded.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //display in photo gallery
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).setData(Uri.fromFile(file)));
         }
     }
 }

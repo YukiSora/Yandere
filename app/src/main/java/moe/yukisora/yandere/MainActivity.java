bde9da801affb3d73a2079a0022a68ad66ec7932
yukisora <yukisora2013@gmail.com>
2016-10-04 12:29:34 +0800

    search fragment

--- app/src/main/java/moe/yukisora/yandere/MainActivity.java
+++ app/src/main/java/moe/yukisora/yandere/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.ImageButton;
 import java.io.File;
 
 public class MainActivity extends Activity {
-    private static final int NUM_ITEMS = 4;
+    private static final int NUM_ITEMS = 5;
     private static File directory;
     private static boolean isSafe;
     private static int dpi;
@@ -85,6 +85,7 @@ public class MainActivity extends Activity {
         final ImageButton randomBtn = ((ImageButton)findViewById(R.id.random));
         final ImageButton popularBtn = ((ImageButton)findViewById(R.id.popular));
         final ImageButton settingBtn = ((ImageButton)findViewById(R.id.setting));
+        final ImageButton searchBtn = ((ImageButton)findViewById(R.id.search));
 
         viewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
             @Override
@@ -102,20 +103,22 @@ public class MainActivity extends Activity {
                     case 2:
                         return PostFragment.newInstance("https://yande.re/post/popular_recent.json?page=", false);
                     case 3:
+                        return SearchFragment.newInstance();
+                    case 4:
                         return SettingFragment.newInstance();
                     default:
                         return null;
                 }
             }
         });
-        viewPager.setOffscreenPageLimit(4);
+        viewPager.setOffscreenPageLimit(5);
 
         //dynamic change button image
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             private int lastPosition;
-            private ImageButton[] imageButtons = new ImageButton[]{postBtn, randomBtn, popularBtn, settingBtn};
-            private int[] normalResourceId = new int[]{R.drawable.list, R.drawable.random, R.drawable.rank, R.drawable.setting};
-            private int[] focusedResourceId = new int[]{R.drawable.list_focused, R.drawable.random_focused, R.drawable.rank_focused, R.drawable.setting_focused};
+            private ImageButton[] imageButtons = new ImageButton[]{postBtn, randomBtn, popularBtn, searchBtn, settingBtn};
+            private int[] normalResourceId = new int[]{R.drawable.list, R.drawable.random, R.drawable.rank, R.drawable.search, R.drawable.setting};
+            private int[] focusedResourceId = new int[]{R.drawable.list_focused, R.drawable.random_focused, R.drawable.rank_focused, R.drawable.search_focused, R.drawable.setting_focused};
 
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -155,10 +158,17 @@ public class MainActivity extends Activity {
             }
         });
 
-        settingBtn.setOnClickListener(new View.OnClickListener() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 viewPager.setCurrentItem(3);
+            }
+        });
+
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(4);
             }
         });
     }
---
+++ app/src/main/java/moe/yukisora/yandere/SearchFragment.java
@@ -0,0 +1,29 @@
+package moe.yukisora.yandere;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    public static SearchFragment newInstance() {
+        Bundle args = new Bundle();
+        SearchFragment fragment = new SearchFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return view;
+    }
+}

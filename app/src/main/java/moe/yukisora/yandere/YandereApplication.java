043d19534023d46495a3226a8de88bfa8fadc353
yukisora <yukisora2013@gmail.com>
2017-10-26 21:00:45 +0800

    update android studio

--- app/src/main/java/moe/yukisora/yandere/YandereApplication.java
+++ app/src/main/java/moe/yukisora/yandere/YandereApplication.java
@@ -27,7 +27,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class YandereApplication extends Application {
-    public static final String APPLICATION_FOLDER= "Yandere";
+    public static final String APPLICATION_FOLDER = "Yandere";
     public static final String TAGS_FILENAME = "tags.json";
     public static final String HARMONY_FLAG_FILENAME = "yandere_386449.png";
 
